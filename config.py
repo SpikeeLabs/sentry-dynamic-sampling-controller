f58dbd4ba0f3d09431d1966efad3d04bda178283
jeanloup.monnier <jean-loup.monnier@spikeelabs.fr>
2023-01-18 17:08:58 +0100

    feat: allow using statsd server to export metric

--- config.py
+++ config.py
@@ -1,5 +1,6 @@
 # -- coding: UTF-8 --
 import os
+import socket
 
 worker_class = "gevent"
 workers = int(os.getenv("WORKERS", "4"))
@@ -9,3 +10,10 @@ timeout = os.environ.get("TIMEOUT", 60)
 loglevel = os.environ.get("LOG_LEVEL", "info")
 errorlog = os.environ.get("ERROR_LOG", "-")
 accesslog = os.environ.get("ACCESS_LOG", "-")
+
+
+_statsd_host = os.getenv("STATSD_HOST")
+_statsd_port = os.getenv("STATSD_PORT")
+if _statsd_host and _statsd_port:
+    statsd_host = f"{_statsd_host}:{_statsd_port}"
+    statsd_prefix = socket.gethostname()
